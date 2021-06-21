67051a32de458aa12f1b20932a5ab40093e19524
jw9506 <cf.dota@gmail.com>
2021-06-21 14:59:23 -0400

    add starting code

---
+++ src/java/main/servlets/Home.java
@@ -0,0 +1,22 @@
+package java.main.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class Home extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  }
+}
